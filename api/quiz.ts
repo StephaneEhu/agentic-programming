d10ebb76723623cc0580750141d936fd10539907
EHU Niamian Yannick Stephane <niamianstephane@gmail.com>
2025-08-07 12:46:58 +0200

    Fix EC compatibility

--- api/quiz.ts
+++ api/quiz.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+module.exports= async function handler(req: VercelRequest, res: VercelResponse) {
   const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
   if (!OPENAI_API_KEY) {
